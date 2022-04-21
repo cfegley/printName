74e2a9c37340da459048e1ec25b51f9abad59e48
cfegley <103463191+cfegley@users.noreply.github.com>
2022-04-21 10:07:35 -0700

    Add files via upload

---
+++ printName.py
@@ -0,0 +1,4 @@
+def printName():
+	print("Name: Chad Fegley\nPronouns: He/Him\nFavorite movie: My favorite movie has to be The Dark Knight.\nI really liked the Joker in that movie and the action was fantastic.\nThe Joker in that movie is forever immortalized as one of the best acting performances ever.")
+
+printName()
